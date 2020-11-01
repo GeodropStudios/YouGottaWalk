6bab77c8cb8a4b97b25d612827173c6a45f3ac11
Anders Steen <anders.steen98@gmail.com>
2020-11-02 00:35:00 +0100

    implement graph

---
+++ YouGottaWalk/Assets/Scripts/Graph.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Graph<T> {
+
+    private List<List<int>> neighborList;
+    private List<T> nodes;
+
+    public Graph() {
+        neighborList = new List<List<int>>();
+        nodes = new List<T>();
+    }
+
+    public List<int> GetNeighbors(int node) {
+        return neighborList[node];
+    }
+
+    public T GetElement(int node) {
+        return nodes[node];
+    }
+
+    public void AddNode(T element) {
+        nodes.Add(element);
+    }
+
+    public void AddEdge(int from, int to) {
+        if (neighborList[from] == null) {
+            neighborList[from] = new List<int>();
+        } else {
+            neighborList[from].Add(to);
+        }
+    }
+}
